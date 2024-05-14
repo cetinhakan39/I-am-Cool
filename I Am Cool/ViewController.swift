15364b5004deae46774b4229250cf0ddf9125331
cetinhakan39 <cetinhakan39@gmail.com>
2024-05-14 21:06:41 +0300

    Initial Commit

---
+++ I Am Cool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Cool
+//
+//  Created by Hakan Çetin on 7.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
